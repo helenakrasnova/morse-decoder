ce06e0d568f78032a2d4261918535859adc7b70a
Helena Krasnova <alenka5354@mail.ru>
2020-10-07 15:38:28 +0300

    feat: add morse-decoder task

--- src/index.js
+++ src/index.js
@@ -1,44 +1,87 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    let binaryArr = [];
+    let morseArr = [];
+    let binaryStart = 0;
+    let binaryEnd = 10;
+    let result = '';
+
+    for (i = 0; i < expr.length / 10; i++) {
+        if (expr.slice(binaryStart, binaryEnd) === '**********') {
+            binaryArr.push(' ');
+        }
+        else {
+            binaryArr.push(expr.slice(binaryStart, binaryEnd));
+        }
+        binaryStart += 10;
+        binaryEnd += 10;
+    }
+
+    for (let i = 0; i < binaryArr.length; i++) {
+        let morseStart = 0;
+        let morseEnd = 2;
+        let subStr = '';
+        for (let j = 0; j < binaryArr[i].length / 2; j++) {
+            if (binaryArr[i].slice(morseStart, morseEnd) === '11') {
+                subStr += '-';
+            }
+            else if (binaryArr[i].slice(morseStart, morseEnd) === '10') {
+                subStr += '.';
+            }
+            morseStart += 2;
+            morseEnd += 2;
+        }
+        morseArr.push(subStr);
+    }
+
+    for (let i = 0; i < morseArr.length; i++) {
+        if (morseArr[i] === '') {
+            result += ' ';
+        }
+        else {
+            result += MORSE_TABLE[morseArr[i]];
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
